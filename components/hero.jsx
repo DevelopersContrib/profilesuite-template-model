281d8d10f81b9e5e3dbecc165c23fa4e65ccbe7a
Andre Bon Cipriano <andreboncipriano@gmail.com>
2024-10-15 14:47:30 +0800

    Updates
    - Hero Section
    - CSS

--- components/hero.jsx
+++ components/hero.jsx
@@ -5,16 +5,19 @@ export default function Hero({ profile, gallery }) {
     <section
       className="hero-section"
       style={{
-        backgroundImage: `url('${gallery.length>0 ? 'https://www.profilesuite.com/uploads/gallery/'+gallery[0].filename : 'https://www.profilesuite.com/uploads/profile/'+profile?.profile_image}')`,
+        backgroundImage: `url('${gallery.length > 0 ? 'https://www.profilesuite.com/uploads/gallery/' + gallery[0].filename : 'https://www.profilesuite.com/uploads/profile/' + profile?.profile_image}')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
+        position: 'relative',
       }}
     >
+      <div className="hero-overlay"></div>
+      
       <div className="container container-padding">
         <div className="row align-items-center gy-6 gy-xl-0">
           <div className="col-md-6">
             <h1 className="display-2 mb-4">{profile.name}</h1>
-            <p className='lead'>{profile.slogan}</p>
+            <p className="lead">{profile.slogan}</p>
           </div>
           <div className="col-md-6 text-center">
             <div className="shape-wrapper">
